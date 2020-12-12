6ed249d64fa217c3e145d5203d29648f5ea588ff
marcfg <marc9223@gmail.com>
2020-12-12 18:48:44 +0100

    modelos vista anfitriones y viajeros guardados

--- models/AnfitrionGuardado.js
+++ models/AnfitrionGuardado.js
@@ -4,9 +4,21 @@ module.exports = (sequelize, DataTypes) => {
   const AnfitrionGuardado = sequelize.define('AnfitrionGuardado', {
    
     usuario_id: DataTypes.INTEGER,
-    anfitrion_id: DataTypes.INTEGER
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: false
+    },
+    pais_id: DataTypes.INTEGER,
+    nombre: DataTypes.STRING,
+    apellidos: DataTypes.STRING,
+    nacimiento: DataTypes.DATE,
+    foto: DataTypes.STRING,
+    estrellitas: DataTypes.INTEGER,
+    ciudad: DataTypes.STRING,
+    descripcion: DataTypes.STRING
   
-  }, { tableName: 'anfitriones_guardados', timestamps: false });
+  }, { tableName: 'vista_anfitriones_guardados', timestamps: false });
   
   return AnfitrionGuardado;
 };
--- models/ViajeroGuardado.js
+++ models/ViajeroGuardado.js
@@ -4,9 +4,22 @@ module.exports = (sequelize, DataTypes) => {
   const ViajeroGuardado = sequelize.define('ViajeroGuardado', {
    
     usuario_id: DataTypes.INTEGER,
-    anfitrion_id: DataTypes.INTEGER
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: false
+    },
+    pais_id: DataTypes.INTEGER,
+    nombre: DataTypes.STRING,
+    apellidos: DataTypes.STRING,
+    foto: DataTypes.STRING,
+    nacimiento: DataTypes.DATE,
+    descripcion: DataTypes.STRING,
+    fecha: DataTypes.DATE,
+    presupuesto: DataTypes.INTEGER,
+    estrellitas: DataTypes.INTEGER
   
-  }, { tableName: 'viajeros_guardados', timestamps: false });
+  }, { tableName: 'vista_viajeros_guardados', timestamps: false });
   
   return ViajeroGuardado;
 };
